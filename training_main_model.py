021711309c82c9be046b768659b81b615c6e0102
Kinjal-16 <bhattacharyyakinjal@gmail.com>
2022-12-03 18:36:57 +0000

    Changed Parameters and added report

--- training_main_model.py
+++ training_main_model.py
@@ -4,10 +4,12 @@ from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import keras.optimizers
 from tensorflow.keras import layers, regularizers
 import tensorflow as tf
 from keras.models import Sequential
@@ -25,7 +27,7 @@ npar=df.iloc[:,:-1]
 print(npar)
 X=fit.fit_transform((np.array(df.iloc[:,:-1], dtype=float)))
 
-X_train,X_test,Y_train,Y_test=train_test_split(X,y,test_size=0.33)
+X_train,X_test,Y_train,Y_test=train_test_split(X,y,test_size=0.30)
 print(len(Y_train))
 print(len(Y_test))
 
@@ -35,7 +37,8 @@ def trainModel(model, epochs, optimizer):
     return model.fit(X_train,Y_train,validation_data=(X_test,Y_test),epochs=epochs,batch_size=batch_size)
 def plotV(history):
     print("Validation Accuracy",max(history.history["val_accuracy"]))
-    pd.DataFrame(history.history).plot(figsize=(12,6))
+    pd.DataFrame(history.history).plot(figsize=(12,6),)
+    plt.xlabel('epoch')
     plt.show()
 
 
@@ -49,15 +52,28 @@ model = k.models.Sequential([
     k.layers.Dense(128, activation="relu"),
     k.layers.Dropout(0.2),
 
-    k.layers.Dense(64, activation="relu"),
+    k.layers.Dense(128, activation="relu"),
     k.layers.Dropout(0.2),
+
     k.layers.Dense(10, activation="softmax"),
 ])
 
-print(model.summary())
+opt = keras.optimizers.Adam(learning_rate=0.0001)
+model_history = trainModel(model = model, epochs = 60, optimizer=opt)
 
-model_history = trainModel(model = model, epochs = 800, optimizer='adam')
-test_loss,test_acc = model.evaluate(X_test,Y_test,batch_size=128)
-print("the test loss is", test_loss)
-print("\n accuracy =", test_acc*100)
+print(model.summary())
+preds = model.predict(X_train)
+y_pred = np.argmax(preds, axis=1)
+y_train1 = Y_train
+print(classification_report(y_train1, y_pred)
+)
+print(confusion_matrix(y_train1, y_pred)
+)
+preds = model.predict(X_test)
+y_pred = np.argmax(preds, axis=1)
+y_test1 = Y_test
+print(classification_report(y_test1, y_pred)
+)
+print(confusion_matrix(y_test1, y_pred)
+)
 plotV(model_history)
